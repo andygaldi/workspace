a30b502db805fb3a896d98718f98ca8e6a367cbf
andygaldi <agaldi@phillies.com>
2019-02-27 18:30:50 -0500

    components project

---
+++ components/src/index.js
@@ -0,0 +1,45 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import faker from "faker";
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
+
+const App = () => {
+	return (
+		<div className="ui containter comments">
+			<ApprovalCard>
+				<div>
+					<h4>Warning!</h4>
+					Are you sure you want to do this?
+				</div>
+			</ApprovalCard>
+
+			<ApprovalCard>
+				<CommentDetail
+					author="Sam"
+					timeAgo="Today at 4:45PM"
+					comment="Hi"
+					avatar={faker.image.avatar()}
+				/>
+			</ApprovalCard>
+			<ApprovalCard>
+				<CommentDetail
+					author="Alex"
+					timeAgo="Today at 2:00AM"
+					comment="Hey"
+					avatar={faker.image.avatar()}
+				/>
+			</ApprovalCard>
+			<ApprovalCard>
+				<CommentDetail
+					author="Jane"
+					timeAgo="Yesterday at 5:00PM"
+					comment="Yo"
+					avatar={faker.image.avatar()}
+				/>
+			</ApprovalCard>
+		</div>
+	);
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
